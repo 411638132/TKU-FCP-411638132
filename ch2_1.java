da810267e67cee7a3e72626c38d52fdc815129d9
411638132 <411638132@o365.tku.edu.tw>
2024-07-02 13:39:12 +0800

    Add files via upload

---
+++ ch2_1.java
@@ -0,0 +1,8 @@
+public class ch2_1{
+    public static void main(String args[]){
+        int num1 = 20;
+        int num2 = 10;
+        System.out.println(num1+"dog are running");
+        System.out.println(num2+"cat are running");
+    }
+}
