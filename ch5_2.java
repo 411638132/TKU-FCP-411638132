ddd83a25bfbfa94ccbee99ac81dc76712da28031
411638132 <411638132@o365.tku.edu.tw>
2024-07-09 15:22:54 +0800

    Add files via upload

---
+++ ch5_2.java
@@ -0,0 +1,9 @@
+public class ch5_2{
+    public static void main (String[] args){
+        int a=50;
+        if(a%2==0)
+            System.out.printf("%d is an even number",a);
+        else
+            System.out.printf("%d is an odd number",a);
+        }
+    }
